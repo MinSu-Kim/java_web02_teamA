4dbf9627c91d1e1f3b793ac1a895fb51e947da3c
lenovo <lenovo@DESKTOP-CIIGDBG>
2019-07-23 19:14:27 +0900

    guest insert select test 완료

---
+++ src/test/java/kr/or/yi/hairshop/dao/GuestMapperTest.java
@@ -0,0 +1,51 @@
+package kr.or.yi.hairshop.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import kr.or.yi.hairshop.AbstractTest;
+import kr.or.yi.hairshop.dto.Guest;
+
+public class GuestMapperTest extends AbstractTest{
+private static GuestMapper guestDao;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		guestDao=new GuestMapperImpl();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		guestDao=null;
+	}
+	
+	@Test
+	public void test01SelectGuestByAll() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		List<Guest> list = guestDao.selectGuestByAll();
+		log.debug(list.toString());
+		Assert.assertNotNull(list);
+	}
+	@Test
+	public void test02InsertGuest() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Guest guest = new Guest();
+		guest.setgNo(5);
+		guest.setgName("구구까까");
+		guest.setgTel("010-0000-0000");
+		guest.setgEmail("zlzlzl@zlzl.com");
+		guest.setgBirth(new Date());
+		guest.setgJoin(new Date());
+		guest.setgMemo("zzzlzlzlzlz");
+		int res = guestDao.insertGuest(guest);
+		log.debug(guest.toString());
+		Assert.assertEquals(1, res);
+	}
+
+
+}
