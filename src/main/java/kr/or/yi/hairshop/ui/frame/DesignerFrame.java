e399cca071848b1ea60be7ca2c03b97e25c4e0a2
김다영 <yohjk079@naver.com>
2019-07-31 16:51:43 +0900

    데이터삽입->product.designer.guest

--- src/main/java/kr/or/yi/hairshop/ui/frame/DesignerFrame.java
+++ src/main/java/kr/or/yi/hairshop/ui/frame/DesignerFrame.java
@@ -73,7 +73,7 @@ public class DesignerFrame extends JFrame implements ActionListener {
 	
 		ds = new DesignerMapperImpl();
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 840, 624);
+		setBounds(100, 100, 900, 624);
 		contentPane = new JPanel();
 		contentPane.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		contentPane.setLayout(new BorderLayout(0, 0));
